7631936fa26f23e7e9ea4b55623f44665483437e
Evgeny Kabantsev <evgeny.kabantsev@acronis.com>
2019-12-25 19:06:08 +0200

    added first demo logic to RSS application

--- app/main.go
+++ app/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/toorosan/rss/pkg/rssreader"
+)
 
 const (
 	// appName defines application name
@@ -8,6 +13,22 @@ const (
 )
 
 func main() {
-	defer fmt.Printf("%q is stopped\n", appName)
 	fmt.Printf("%q initiated\n", appName)
+	if RssDemo() {
+		defer os.Exit(1)
+	}
+	fmt.Printf("%q is stopped\n", appName)
+}
+
+func RssDemo() bool {
+	urls := []string{"https://www.feedforall.com/sample.xml", "https://www.feedforall.com/sample-feed.xml"}
+	results, err := rssreader.Parse(urls)
+	for _, item := range results {
+		println(item.String())
+	}
+	if err != nil {
+		println("following errors occured:\n", err.Error())
+	}
+
+	return err != nil
 }
